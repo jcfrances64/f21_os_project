f53dc34e784c9fc0194104f6ed42f7ccc5d36db3
Jake <jcfrnaces64@gmail.com>
2021-11-07 12:35:49 -0600

    main commit

--- edu/cs300/DebugLog.java
+++ edu/cs300/DebugLog.java
@@ -2,15 +2,15 @@ package edu.cs300;
 
 
 public class DebugLog {
- public final static boolean DEBUG = true;    
- public static void log(String message) {
-  if (DEBUG) {
-    String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
-    String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
-    String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-    int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
+  public final static boolean DEBUG = true;    
+    public static void log(String message) {
+      if (DEBUG) {
+        String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
+        String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
+        String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+        int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
 
-    System.err.println(className + "." + methodName + "():" + lineNumber+" "+ message);
+        System.err.println(className + "." + methodName + "():" + lineNumber+" "+ message);
+    }
   }
- }
-}
+} //edit
