c95b2adf3acec5072041d13f1297e679796c06b0
Jake <jcfrnaces64@gmail.com>
2021-11-08 04:12:59 -0600

    implemented sigint handler and thread

--- process_records.c
+++ process_records.c
@@ -11,8 +11,8 @@
 #include <pthread.h>
 
 
-int recordsRead;
-int threadCount;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct countStructure {
     int threadID;
@@ -21,20 +21,39 @@ typedef struct countStructure {
 } countStruct;
 
 static void signalHandler(int sig) {
-    printf("Called sigint!\n");
-
+    pthread_cond_signal(&cv);
+    // printf("Called sigint!\n");
     signal(SIGINT, signalHandler);
 
 }
 
-// void printStatusReport(countStruct *reportCountStruct) {
+void *printStatusReport(void *reportStatusStruct) {
+    
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cv, &mutex);
+
+    printf("***Report***\n");
+
+
+
+
+    pthread_mutex_unlock(&mutex);
+
+
+}
+
+// void printStatusReport(countStruct *reportCountStruct, int recordsRead, int threadCount) {
 //     printf("***Report***\n");
-//     printf("%i records read for %i reports\n");
+//     printf("%i records read for %i reports\n", recordsRead, threadCount);
 //     int i;
 //     for(i = 0; i < threadCount; i++) {
-//         printf("Records sent for report index ")
+//         printf("Records sent for report index %i: %i\n", reportCountStruct[i].threadID, reportCountStruct[i].recordCount);
 //     }
 // }
+
+
+
+
 
 
 
@@ -47,25 +66,27 @@ int main(int argc, char**argv) {
     report_record_buf sbuf;
     size_t buf_length;
     countStruct * reportCountStruct;
-
-    recordsRead = 0;
-    // pthread_t statusReportThread;
-    // signal(SIGINT, signalHandler);
-    // pthread_create(&statusReportThread, NULL, (void *) printStatusReport, NULL);
-
-
+    int rc;
     
+    
+
+
+
+    int threadCount = 0;
+    int recordsRead = 0;
 
 
     
 
     int threadIndex;
-    int threadCount = 0;
+    // int threadCount = 0;
     int currentThread;
     // bool threadCountInitialized;
     // char searchString[SEARCH_STRING_FIELD_LENGTH];
 
     // int structIndex = 0;
+
+    //  retrieve report requests, save into countstruct
     do {
         key = ftok(FILE_IN_HOME_DIR,QUEUE_NUMBER);
         if (key == 0xffffffff) {
@@ -108,10 +129,13 @@ int main(int argc, char**argv) {
             fprintf(stderr, "Error initializing count structure\n");
         }
 
-        currentThread = rbuf.report_idx;
 
-        reportCountStruct[currentThread].threadID = rbuf.report_idx;
-        strcpy(reportCountStruct[currentThread].searchString, rbuf.search_string);
+
+        currentThread = rbuf.report_idx;
+        // printf("Thread %i\n" + currentThread);
+
+        reportCountStruct[currentThread - 1].threadID = rbuf.report_idx;
+        strcpy(reportCountStruct[currentThread - 1].searchString, rbuf.search_string);
         // reportCountStruct;
         // structIndex++;
 
@@ -122,22 +146,51 @@ int main(int argc, char**argv) {
     } while(threadIndex > 0);
 
 
-    // int i;
-    // for(i = 0; i < threadCount; i++) {
-    //     reportCountStruct[i].recordCount = 0;
-    //     reportCountstruct[i].
-    // }
+  
+
+
+        // printf("Before thread initialization\n");
+        // pthread_mutex_unlock(&mutex);
+        // start status report thread
+        pthread_t statusReportThread;
+        signal(SIGINT, signalHandler);
+        pthread_create(&statusReportThread, NULL, (void *) printStatusReport, NULL);
+        // printf("After thread initialization\n");
+
+
+        //   delete later
+
+
+
+        // if ((msqid = msgget(key, msgflg)) < 0) {
+        //     int errnum = errno;
+        //     fprintf(stderr, "Value of errno: %d\n", errno);
+        //     perror("(msgget)");
+        //     fprintf(stderr, "Error msgget: %s\n", strerror( errnum ));
+        // }
+        // else
+        //     fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
+
+
+        // do {
+        //     ret = msgrcv(msqid, &rbuf, sizeof(rbuf), 1, 0);    //   receive type 1 message
+
+        //     int errnum = errno;
+        //     if (ret < 0 && errno !=EINTR){
+        //         fprintf(stderr, "Value of errno: %d\n", errno);
+        //         perror("Error printed by perror");
+        //         fprintf(stderr, "Error receiving msg: %s\n", strerror( errnum ));
+        //     }
+        // } while ((ret < 0 ) && (errno == 4));
+
+    //delete
+
+
+
     
-    printf("here\n");
-    int j;
-    for(j = 0;)
 
+    //  retrieve records
 
-    //initialize thread
-
-
-
-    /*
     FILE *recordFile = fopen("records.mini", "r");   //  change to be the value from stdin, was f
     sbuf.mtype = 2; //  used to exist in do while
     char record[RECORD_FIELD_LENGTH]; // was str
@@ -150,7 +203,7 @@ int main(int argc, char**argv) {
         for(i = 0; i < threadCount; i++) {
             if(strstr(record, reportCountStruct[i].searchString) != 0) {
 
-                printf("Record Matched on thread: %i %s\n", reportCountStruct[i].threadID, record);
+                // printf("Record Matched on thread: %i %s\n", reportCountStruct[i].threadID, record);
 
                 key = ftok(FILE_IN_HOME_DIR, reportCountStruct[i].threadID);
                 if (key == 0xffffffff) {
@@ -172,8 +225,10 @@ int main(int argc, char**argv) {
                 // sbuf.mtype = 2;
                 //  send message
                 //or add lock here
+
+                pthread_mutex_lock(&mutex);
                 reportCountStruct[i].recordCount++;
-                recordsRead++;
+                // recordsRead++;
                 if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
                     int errnum = errno;
                     fprintf(stderr,"%d, %ld, %s %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
@@ -184,11 +239,19 @@ int main(int argc, char**argv) {
                 else
                     fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
 
+
+                pthread_mutex_unlock(&mutex);
                 //remove lock
                     // printf("Sent %s to thread %i\n", record, threadIndex);
             }
         }
-        recordsRead++;
+
+        if(strcmp(record, "\n") != 0) {
+            pthread_mutex_lock(&mutex);
+            recordsRead++;
+            pthread_mutex_unlock(&mutex);
+        }
+
     } while(strcmp(record, "\n") != 0);
 
     int i;
@@ -226,7 +289,11 @@ int main(int argc, char**argv) {
             fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
 
     }
-    */
+
+    // printf("Thread count %s\n", threadCount);
+    // printf("Records read: %s\n", recordsRead);
+    // printStatusReport(reportCountStruct, recordsRead, threadCount);
+    
 
 
  
